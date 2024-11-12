de970005d16f6f8f2a7970036f1bd2c6d77bf6ed
PENDE <enangpendemma@gmail.com>
2024-11-12 14:44:03 +0100

    Added the two login files and removed // from th js file, line 13 and 17

---
+++ javaScript.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>My First JavaScript</h2>
+
+<button type="button"
+onclick="document.getElementById('demo').innerHTML = Date()">
+Click me to display Date/Time.</button>
+
+<p id="demo"></p>
+
+How to create variables:
+var x;
+let y;
+
+ How to use variables:
+x = 5;
+y = 6;
+let z = x + y;
+</body>
+</html> 
